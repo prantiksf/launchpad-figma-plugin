bcaf5abeb78906df141df949c81a169217e2b241
Prantik Banerjee <prantik.banerjee@prantik-ltm2chv.internal.salesforce.com>
2026-01-20 21:58:47 +0530

    Add 'Create Pages' option to dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -1446,6 +1446,15 @@ export function App() {
                           </svg>
                           Add pattern
                         </button>
+                        <button 
+                          className="header__dropdown-menu-item"
+                          onClick={() => { setView('scaffold'); setShowMoreMenu(false); }}
+                        >
+                          <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor">
+                            <path d="M2 2h12v12H2V2zm1 1v10h10V3H3zm1 1h8v1H4V4zm0 2h8v1H4V6zm0 2h8v1H4V8zm0 2h5v1H4v-1z"/>
+                          </svg>
+                          Create Pages
+                        </button>
                         <div className="header__dropdown-divider"></div>
                         <button 
                           className="header__dropdown-menu-item"
